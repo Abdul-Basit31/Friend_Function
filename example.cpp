7d3c035ddfa3bbf6736f051d12507ed4f661c25a
adulbasit <bassitb50@gmail.com>
2023-06-11 13:39:37 +0500

    Second

--- example.cpp
+++ example.cpp
@@ -1,1 +1,31 @@
-nbjhbuighuihfuibbxjbjjk
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class B;
+class A
+{
+private:
+	int a;
+public:
+	friend void fun(A,B);
+};
+
+class B
+{
+private:
+	int b;                                     
+public:
+	friend void fun(A,B);
+};
+void fun(A o1, B o2)
+{
+	cout<<"sum of :  "<<o1.a+o2.b;
+}
+
+int main()
+{
+	A obj1;
+	B obj2;
+	fun(obj1,obj2);
+}
